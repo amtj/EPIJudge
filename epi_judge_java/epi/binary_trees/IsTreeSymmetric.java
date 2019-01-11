16d767ffa077303df5be87d13067b113c88083c3
Amitoj <12820331+amtj@users.noreply.github.com>
2019-01-11 23:48:46 +0530

    feat: add IsTreeSymmetric

--- epi_judge_java/epi/binary_trees/IsTreeSymmetric.java
+++ epi_judge_java/epi/binary_trees/IsTreeSymmetric.java
@@ -8,8 +8,30 @@ public class IsTreeSymmetric {
   @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
   public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    if (tree == null) {
+      return true;
+    }
+    else {
+      return isSymmetric(tree.left, tree.right);
+    }
+  }
+
+  public static boolean isSymmetric(BinaryTreeNode<Integer> tree1, BinaryTreeNode<Integer> tree2) {
+    if (tree1 == null && tree2 == null) {
+      return true;
+    }
+
+    else if (tree1 == null || tree2 == null) {
+      return false;
+    }
+    else {
+      if (!tree1.data.equals(tree2.data)) {
+        return false;
+      }
+      else {
+        return isSymmetric(tree1.left, tree2.right) && isSymmetric(tree1.right, tree2.left);
+      }
+    }
   }
 
   public static void main(String[] args) {
