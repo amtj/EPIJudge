3b1b21cf5600d0bf18e4227ceebd3e2b7e1d1126
Amitoj <12820331+amtj@users.noreply.github.com>
2019-01-11 19:26:03 +0530

    feat: add IsTreeBalanced

--- epi_judge_java/epi/binary_trees/IsTreeBalanced.java
+++ epi_judge_java/epi/binary_trees/IsTreeBalanced.java
@@ -9,8 +9,48 @@ public class IsTreeBalanced {
   @EpiTest(testDataFile = "is_tree_balanced.tsv")
 
   public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    if (tree == null) {
+      return true;
+    }
+
+    calculateHeight(tree);
+
+    if (tree.left == null && tree.right == null) {
+      return true;
+    }
+    else if (tree.right == null) {
+      return tree.left.data == 0;
+    }
+    else if (tree.left == null) {
+      return tree.right.data == 0;
+    }
+    else {
+      if (Math.abs(tree.left.data - tree.right.data) > 1 ) {
+        return false;
+      }
+      else {
+        return isBalanced(tree.left) && isBalanced(tree.right);
+                                                  }
+    }
+  }
+
+  public static void calculateHeight(BinaryTreeNode<Integer> tree) {
+    if (tree.left == null && tree.right == null) {
+       tree.data = 0;
+    }
+    else if (tree.right == null) {
+      calculateHeight(tree.left);
+      tree.data = tree.left.data + 1;
+    }
+    else if (tree.left == null) {
+      calculateHeight(tree.right);
+      tree.data = tree.right.data + 1;
+    }
+    else {
+      calculateHeight(tree.left);
+      calculateHeight(tree.right);
+      tree.data = Math.max(tree.left.data, tree.right.data) + 1;
+    }
   }
 
   public static void main(String[] args) {
