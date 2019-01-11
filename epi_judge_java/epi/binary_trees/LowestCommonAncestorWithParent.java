b8d680a2845a967585dfb6d2e3cadb4e9eaff97f
Amitoj <12820331+amtj@users.noreply.github.com>
2019-01-12 01:00:53 +0530

    feat: add LowestCommonAncestorWithParent

--- epi_judge_java/epi/binary_trees/LowestCommonAncestorWithParent.java
+++ epi_judge_java/epi/binary_trees/LowestCommonAncestorWithParent.java
@@ -1,16 +1,39 @@
-package epi;
+package epi.binary_trees;
+
+import epi.BinaryTree;
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
+
+import java.util.HashSet;
+
 public class LowestCommonAncestorWithParent {
 
   public static BinaryTree<Integer> LCA(BinaryTree<Integer> node0,
                                         BinaryTree<Integer> node1) {
-    // TODO - you fill in here.
-    return null;
+
+    if (node0 == null || node1 == null) {
+      return null;
+    }
+
+    HashSet<BinaryTree<Integer>> set = new HashSet<>();
+
+    set.add(node0);
+    while (node0.parent != null) {
+      set.add(node0.parent);
+      node0 = node0.parent;
+    }
+    set.add(node0);
+
+    while (!set.contains(node1)) {
+      node1 = node1.parent;
+    }
+
+    return node1;
   }
+
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
   public static int lcaWrapper(TimedExecutor executor, BinaryTree<Integer> tree,
                                Integer key0, Integer key1) throws Exception {
