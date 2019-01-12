fa9359959c7dbe107ed7a5c6b400582d7ec44f37
Amitoj <12820331+amtj@users.noreply.github.com>
2019-01-12 19:28:25 +0530

    feat: add SumRootToLeaf

--- epi_judge_java/epi/binary_trees/SumRootToLeaf.java
+++ epi_judge_java/epi/binary_trees/SumRootToLeaf.java
@@ -8,8 +8,19 @@ public class SumRootToLeaf {
   @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
   public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return 0;
+    return sumRoot(tree, "");
+  }
+
+  public static int sumRoot(BinaryTreeNode<Integer> tree, String currentBin) {
+    if (tree == null) {
+      return 0;
+    }
+    else if (tree.left == null && tree.right == null) {
+      return Integer.parseInt(currentBin + tree.data, 2);
+    }
+    else {
+      return sumRoot(tree.left, currentBin + tree.data) + sumRoot(tree.right, currentBin + tree.data);
+    }
   }
 
   public static void main(String[] args) {
